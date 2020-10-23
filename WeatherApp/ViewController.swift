cb93997a19185e3616c97b46d1f7f7772d71690a
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2020-10-23 13:05:12 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Michael Strand on 23/10/2020.
+//  Copyright © 2020 Michael Strand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
