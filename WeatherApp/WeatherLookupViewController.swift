53a11ae7185fd82915afbb53daf1bc42382a2b23
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2020-11-08 11:41:28 -0800

    Split view to improve UI on ipad

---
+++ WeatherApp/WeatherLookupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherLookupViewController.swift
+//  WeatherApp
+//
+//  Created by Michael Strand on 08/11/2020.
+//  Copyright © 2020 Michael Strand. All rights reserved.
+//
+
+import UIKit
+
+class WeatherLookupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
